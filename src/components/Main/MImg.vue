c8922d77ac5b05560642b1b204d8687f88dd2b74
alanmatw <alanmatw@gmail.com>
2020-02-14 15:04:29 +0530

    create main component structure

---
+++ src/components/Main/MImg.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="wrapper">
+     <div class="MainImg">
+     </div>  
+   </div>   
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+  .MainImg {
+     
+  }
+</style>
