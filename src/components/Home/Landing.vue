1ec4aca12069dbcd8890e20507dc50bb5e2f6fd3
alanmatw <alanmatw@gmail.com>
2020-05-01 21:35:37 +0530

    feat(landing): add more info

--- src/components/Home/Landing.vue
+++ src/components/Home/Landing.vue
@@ -1,9 +1,20 @@
 <template>
   <div class = "Main">
     <div class="Base">
-      <h1>Hey there,</h1>
-      <h2>I'm <span id="Lname">ALAN MATHEW</span>, loves to code, Vue.js and Node.js</h2>
-      <h2>Watch a lot of movies....</h2>    
+      <!--<div class="Mtag"><p>:<span id="Mt">\</span><span id="At">></span><span id="Mt">_</span>Hey there</p></div>-->
+      <div class="Top">
+        <div class="Mtag"><p>_Hey there</p></div>
+        <div class="Img"></div>
+      </div>
+
+      <div class="Stag"><p>I'm <span id="Lname">Alan Mathew</span> and I love <span id="Cd">CODING_</span></p></div>
+      <div class="lifeMotto">
+        <div class="HeartL"></div>
+        <div class="Btm"><p>LifeMotto() { while ( ! ( succeed = try() ) ); }</p></div>
+        <div class="HeartR"></div>
+      </div>
+      
+      <!--<div class="Btm"><p>bE HapPpY :)</p></div>-->
     </div>
   </div>       
 </template>
@@ -17,29 +28,85 @@ export default {
 <style scoped>
   .Base {
     box-sizing: border-box;
-    background-color: #1e2022;
+    background-color: #000;
     width: 100%;
     height: 79vh;
-    padding: 50px;
+    padding-top: 10px;
+    padding-left: 80px;
   }
-  h1 {
-    color: #c9d6df;
-    font-family: 'FiraCode-Light';
-    font-size: 50px;
-    padding-bottom: 40px;
+  .Top {
+    display: flex;
   }
-  h2 {
+  .Mtag {
     color: #fff;
-    padding-top: 0px;
+    font-family: 'FiraCode-Light';
+    font-size: 100px;
+    margin-top: 100px;
+    margin-left: 50px;
+    
+  }
+  .Img {
+    background-image: url('../../assets/lql.png');
+    width: 80px;
+    height: 80px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    margin-left: 20px;
+    top: 160px;    
+  }
+  #Mt {
+    margin-left: -30px;;
+  }
+  #At {
+    margin-left: -10x;;
+  }
+  .Stag {
+    color: #fff;
     font-size: 25px;
     font-family: 'FiraCode-Regular';
+    margin-top: -40px;
+    margin-left: 200px;
   }
   #Lname {
-    font-size: 40px;
-    color: #52616b;
+    font-size: 50px;
+    color: #fff;
   }
-  #Lpath {
-    font-size: 35px;
-    color: #000;
+  #Cd {
+    margin-top: -5px;
+    font-size: 120px;
+    color: #fff;
   }
+  .lifeMotto {
+    display: flex;
+  }
+  .Btm {
+    font-family: 'FiraCode-Regular';
+    font-size: 20px;
+    margin-top: 100px;
+    margin-left: 300px;
+    color: #fff;
+  }
+  .HeartL {
+    background-image: url('../../assets/heart.jpg');
+    width: 50px;
+    height: 50px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    left: 300px;
+    top: 80px;  
+  }
+  .HeartR {
+    background-image: url('../../assets/heart.jpg');
+    width: 50px;
+    height: 50px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    top: 80px;  
+  }  
 </style>
