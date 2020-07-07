cf650c66a9c4f0154d976f6ad8f65066ddf37b6f
alanmatw <alanmatw@gmail.com>
2020-07-08 00:57:30 +0530

    feat: create home component

--- client/src/components/Base/baseStruct.vue
+++ client/src/components/Base/baseStruct.vue
@@ -5,17 +5,9 @@
         
       </div>
       <div class="right-body">
-        <div class="one">
-          <div class="one-hdn"></div>
-          <div class="one1-bdy">
-            <p>I'am Alan Mathew,</p>   
-            <p>Web Developer.</p>             
-          </div>    
-          <div class="one2-bdy">
-            <p>MEVN Stack Dev | Cloud Enthusiasist</p>
-          </div>
-        </div>
-        <!--<div class="me"></div>-->
+        <router-view>
+          
+        </router-view>
       </div>
     </div>
   </div>
@@ -54,46 +46,52 @@ export default {
     background-color: #212121;
     /*background-color: #323232;*/
   }
-  .one {
-    color: #fff;
-    margin-left: 10%;
-    margin-top: 12%;
-  }
-  .one-hdn {
-    width: 300px;
-    height: 150px;
-    background-image: url('../../assets/Text.svg');
-    /*filter: brightness(0) invert(1);*/
-    filter: invert(82%) sepia(75%) saturate(510%) hue-rotate(95deg) brightness(105%) contrast(106%);
-    background-blend-mode: overlay;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: contain;       
-  }
-  .one1-bdy {
-    margin-top: 5px;
-    font-size: 25px;
-    font-family: FiraCode-Medium;
-  }
-  .one1-bdy p {
-    margin-bottom: 5px;
-  }
-  .one2-bdy {
-    margin-top: 25px;
-    font-size: 23px;
-    font-family: FiraCode-Medium;
-  }  
-  .me {
-    border: 2px solid #fff;
-    position: absolute;
-    top: 5%;
-    left: 50%;
-    width: 400px;
-    height: 400px;
-    background-image: url('../../assets/me.jpg');    
-    background-blend-mode: overlay;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;     
-  }
 </style>
+
+
+      <div class="sidenav">
+        <div class="sidenav-mid">
+
+          <div class="sidenav-mid-one">
+            <router-link :to="{name: ''}" @click.native="fn">
+              <div class="wr-1">
+                <div class="sidenav-mid-img1"></div>
+                <p>Bookings</p>
+              </div>
+            </router-link>
+          </div>
+          <div class="sidenav-mid-two">
+            <router-link :to="{name: ''}" @click.native="fn">
+              <div class="wr-1">
+                <div class="sidenav-mid-img2"></div>
+                <p>Order History</p>
+              </div>
+            </router-link>
+          </div>
+          <div class="sidenav-mid-three">
+            <router-link :to="{name: ''}" @click.native="fn">
+              <div class="wr-1">
+                <div class="sidenav-mid-img3"></div>
+                <p>Favorites</p>
+              </div>
+            </router-link>
+          </div>      
+          <div class="sidenav-mid-four">
+            <router-link :to="{name: ''}" @click.native="fn">
+              <div class="wr-1">
+                <div class="sidenav-mid-img4"></div>
+                <p>My Profile</p>
+              </div>
+            </router-link>
+          </div>   
+          <div class="sidenav-mid-five">
+            <router-link :to="{name: ''}" @click.native="fn">
+              <div class="wr-1">
+                <div class="sidenav-mid-img5"></div>
+                <p>Notifications</p>
+              </div>
+            </router-link>
+          </div>                                 
+        
+        </div>
+      </div>
---
+++ client/src/components/Home/home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="Wrapper">
+    <div class="home-content">
+      <div class="hc-hello"></div>
+      <div class="hc-next">
+        <p>I'am Alan Mathew,</p>   
+        <p>Web Developer.</p>             
+      </div>    
+      <div class="hc-skill">
+          <p>MEVN Stack Dev | Cloud Enthusiasist</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: { 
+    fn() {
+    }
+  }   
+}       
+</script>
+
+<style scoped>
+  .home-content {
+    margin-left: 10%;
+    margin-top: 12%;
+  }
+  .hc-hello {
+    width: 300px;
+    height: 150px;
+    background-image: url('../../assets/Text.svg');
+    /*filter: brightness(0) invert(1);*/
+    filter: invert(82%) sepia(75%) saturate(510%) hue-rotate(95deg) brightness(105%) contrast(106%);
+    background-blend-mode: overlay;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;        
+  }
+  .hc-next {
+    margin-top: 5px;
+    font-size: 25px;
+    font-family: FiraCode-Medium;    
+  }
+  .hc-next p {
+    margin-bottom: 5px;
+  }    
+  .hc-skill {
+    margin-top: 25px;
+    font-size: 23px;
+    font-family: FiraCode-Medium;
+  } 
+  /*.me {
+    border: 2px solid #fff;
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    width: 400px;
+    height: 400px;
+    background-image: url('../../assets/me.jpg');    
+    background-blend-mode: overlay;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;     
+  }*/
+</style>
