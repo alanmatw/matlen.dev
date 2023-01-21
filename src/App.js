bbd35c9197b11d269e8f6577e9a4a43ce4e17efb
alanmatw <alanmatw@gmail.com>
2023-01-21 16:44:39 -0500

    feat: create router structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/home';
+
+import {
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hi, My name is Alan
-        </p>
-      </header>
-    </div>
+      <div className='App'>
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
+      </div>
   );
 }
 
