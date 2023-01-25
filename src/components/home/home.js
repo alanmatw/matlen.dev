1f705c32e06a3381328ae75debccdc76b064fbab
alanmatw <alanmatw@gmail.com>
2023-01-24 21:33:09 -0500

    refactor(home): minor changes in font color

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -48,23 +48,23 @@ export default class homeFn extends React.Component {
                             {/*<div className='wBR-photo'></div>*/}
                             <p className='wBR-p1'>
                                 I am a web developer, specialized in MERN stack. Currently, I live in Kitchener, 
-                                Ontario. Orginally I'm from Kerala, India. I love reading books in my spare time. I favorite book is 'The Lies of Locke Lamora'
+                                Ontario. Orginally I'm from Kerala, India. I love reading books in my spare time. My favorite book is 'The Lies of Locke Lamora'
                                 by Scott Lynch.
                             </p>
                             <p className='wBR-p1'>
-                                <span className='wBR-green'>Projects:</span><br></br><br></br>
-                                <span className='wBR-green'>Zode: </span>I handled the front-end part of Zode using React.js. Zode is a platform where all developers can interact and plan their 
+                                <span className='wBR-bold'>Projects:</span><br></br>
+                                <span className='wBR-bold'>Zode: </span>I handled the front-end part of Zode using React.js. Zode is a platform where all developers can interact and plan their 
                                 projects more efficiently and affordably. Some of the features include: group video calling, instant messaging, calendar integration, 
                                 project tracking, organizing tasks using boards, etc<br></br>
                                 GitHub Link: <span className='wBR-green'>https://github.com/alanmatw/zod-frontend</span>
                                 <br></br><br></br>
 
-                                <span className='wBR-green'>CliqServe: </span>I developed the front-end part of CliqServe (Github Repository Name: codename-prox), using Vue.js. 
+                                <span className='wBR-bold'>CliqServe: </span>I developed the front-end part of CliqServe (Github Repository Name: codename-prox), using Vue.js. 
                                 This project is based on an idea to deliver professional services to people at the comfort of their home.<br></br> 
                                 GitHub Link: <span className='wBR-green'>https://github.com/alanmatw/codename-prox</span>                             
                             </p>
                             <p className='wBR-p1'>
-                                <span className='wBR-green'>Contact Details:</span><br></br><br></br>Email: <span className='wBR-green'>alanmatw@gmail.com</span><br></br>Phone: <span className='wBR-green'>+1 647-928-3049</span>
+                                <span className='wBR-bold'>Contact Details:</span><br></br>Email: <span className='wBR-green'>alanmatw@gmail.com</span><br></br>Phone: <span className='wBR-green'>+1 647-928-3049</span>
                                 <br></br>GitHub: <span className='wBR-green'>https://github.com/alanmatw</span><br></br>linkedin: <span className='wBR-green'>https://www.linkedin.com/in/alanmatw/</span>
                             </p>
                         </div>
