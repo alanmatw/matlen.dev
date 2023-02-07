9695a7641e5267d6dc94636679ae0e6b35e270b6
alanmatw <alanmatw@gmail.com>
2023-02-06 20:45:53 -0500

    refactor(home): change para structure

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -59,13 +59,42 @@ export default class homeFn extends React.Component {
                     <div className='webBodyRight'>
 
                         <div className='wBR-container'>
-                            {/*<div className='wBR-photo'></div>*/}
-                            <p className='wBR-p1'>
-                                I am a web developer, specialized in MERN stack. Currently, I live in Kitchener, 
-                                Ontario. Orginally I'm from Kerala, India. I love reading books and playing PC games. My favorite book is 'The Lies of Locke Lamora'
-                                by Scott Lynch, and my favorite game is 'Assasins Creed'.
-                            </p>
-                            <p className='wBR-p1'>
+                           
+                           <div>
+                                <p className='wBR-s1-p1'>About Me.</p>
+                                <p className='wBR-s1-p2'>
+                                    I am a web developer, specialized in MERN stack. Currently, I live in Kitchener, 
+                                    Ontario. Orginally I'm from Kerala, India. I love reading books and playing PC games. My favorite book is 'The Lies of Locke Lamora'
+                                    by Scott Lynch, and my favorite game is 'Assasins Creed'.
+                                </p>
+                           </div>
+                           
+                           <div>
+                                <p className='wBR-s1-p1'>Skills.</p>
+                                <p className='wBR-s1-p2'>
+                                    As a programmer, I am skilled at Python, C, C++, Javascript. In the case of web development I follow MERN stack (MongoDB, Express.js, React.js, Node.js).
+                                     I am also enthusiastic about ethical hacking and network security, and working on to enhance those skills.
+                                </p>
+                           </div>
+
+                            <div>
+                                <p className='wBR-s1-p1'>Projects.</p>
+                                <p className='wBR-s1-p2'>
+                                    Zode: I handled the front-end part of Zode using React.js. Zode is a platform where all developers can interact and plan their 
+                                    projects more efficiently and affordably. Some of the features include: group video calling, instant messaging, calendar integration, 
+                                    project tracking, organizing tasks using boards, etc<br></br>
+                                    GitHub Link: <a href='https://github.com/alanmatw/zod-frontend' id='wBR-underline'><span className='wBR-white'>https://github.com/alanmatw/zod-frontend</span></a>
+                                </p>
+                                <p className='wBR-s1-p2'>
+                                    CliqServe: I developed the front-end part of CliqServe (Github Repository Name: codename-prox), using Vue.js. 
+                                    This project is based on an idea to deliver professional services to people at the comfort of their home.<br></br>
+                                    GitHub Link: <a href='https://github.com/alanmatw/codename-prox' id='wBR-underline'><span className='wBR-white'>https://github.com/alanmatw/codename-prox</span></a>                                     
+                                </p>
+                            </div>
+                            
+                            
+                            
+                            {/*<p className='wBR-p1'>
                                 <span className='wBR-bold-qw'>Projects:</span><br></br>
                                 <span className='wBR-bold'>Zode: </span>I handled the front-end part of Zode using React.js. Zode is a platform where all developers can interact and plan their 
                                 projects more efficiently and affordably. Some of the features include: group video calling, instant messaging, calendar integration, 
@@ -76,7 +105,7 @@ export default class homeFn extends React.Component {
                                 <span className='wBR-bold'>CliqServe: </span>I developed the front-end part of CliqServe (Github Repository Name: codename-prox), using Vue.js. 
                                 This project is based on an idea to deliver professional services to people at the comfort of their home.<br></br> 
                                 GitHub Link: <a href='https://github.com/alanmatw/codename-prox' id='wBR-underline'><span className='wBR-green'>https://github.com/alanmatw/codename-prox</span></a>                             
-                            </p>
+                            </p>*/}
 
                         </div>
                         <div className='wBR-copyright'>
